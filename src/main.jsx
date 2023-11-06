4daeefd4cb833b6a64da55ecba1739c9b82a5f54
kashyapAmansingh <coder85113@gmail.com>
2023-11-06 17:19:56 +0530

    resolve package

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/Store.jsx'
 import MainSideBar from './Component/Component/MainSideBar.jsx'
-<<<<<<< HEAD
    
-=======
-  
->>>>>>> 439afe43bfeef0a26b450ba7a0d315a866f1101c
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
       <MainSideBar/>
