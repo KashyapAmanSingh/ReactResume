b35a241fe638661fff3b29a740fc65a0e9a5b615
kashyapAmansingh <coder85113@gmail.com>
2023-11-08 20:18:20 +0530

    update- added my-resume page

--- src/Page/Preview.jsx
+++ src/Page/Preview.jsx
@@ -1,17 +1,21 @@
- import { useSearchParams } from 'react-router-dom';
+ import { useLocation, useSearchParams } from 'react-router-dom';
 
 // Import resume templates for different templates
  import ResumeTemplateFirst from '../Component/Templates/template1/resumeTemplate1';
 import ResumeTemplateSecond from '../Component/Templates/template2/resumeTemplate2';
 import ResumeTemplateThird from '../Component/Templates/template3/resumeTemplate';
 import ResumeTemplateFour from '../Component/Templates/template4/ResumeTemplateFour';
-
+import NotFound from './NotFound';
+ 
  // Preview component for displaying selected resume template
 
 const  Preview = () => {
     // Get search parameters from the URL
 
   const [searchParams] = useSearchParams();
+  const location = useLocation()
+  console.log(location.pathname);
+
    const templateNumber = searchParams.get('template');
      // Define a mapping of template numbers to their respective components
    const templates = {
@@ -26,7 +30,16 @@ const  Preview = () => {
 
   return (
     <div>
-       {TemplateComponent}
+      {TemplateComponent}
+       {
+        location.pathname === "/DetailFill/Preview" ? (  
+        <>       
+        <h1 className='mx-5 text-center '>First <span className='text-info'>Select</span> the Template & fill respective <span className='text-info'> Details</span></h1>
+       <NotFound/>
+       </>
+        ): null
+       }
+    
      </div>
   );
 };
